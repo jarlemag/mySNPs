d6e34011460bc54dce25c8f31b0362b286e1c0f0
Jarle Pahr <jarle.pahr@gmail.com>
2013-09-20 21:59:21 +0200

    online tools.

---
+++ onlinetools.py
@@ -0,0 +1,30 @@
+#onlinetools.py
+
+'''
+Functions and code for querying databased and websites.
+
+Includes code from:
+
+http://snpedia.com/index.php/Bulk
+
+'''
+
+from wikitools import *
+
+
+def snpedia_getallIDs():
+    site = wiki.Wiki("http://bots.snpedia.com/api.php") # open snpedia
+    snps = category.Category(site, "Is_a_snp")
+    snpedia = []
+       
+    for article in snps.getAllMembersGen(namespaces=[0]):# get all snp-names as list and print them
+        snpedia.append(article.title.lower())
+        #print article.title
+        print 'List saved.'
+    return snpedia
+
+def snpedia_getfulltext(snp):
+    site = wiki.Wiki("http://bots.snpedia.com/api.php")
+    pagehandle = page.Page(site,snp)
+    snp_page = pagehandle.getWikiText()
+    return snp_page
